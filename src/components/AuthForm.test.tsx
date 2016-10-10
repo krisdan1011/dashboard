475fb20d715dd2bb711fa07dd8432a0e518019c7
krisdan1011 <krisdan001011@gmail.com>
2016-10-10 13:42:06 -0400

    formatting

--- src/components/AuthForm.test.tsx
+++ src/components/AuthForm.test.tsx
@@ -7,32 +7,32 @@ import AuthForm from "./AuthForm";
 describe("AuthForm", () => {
     let renderer: React.ShallowRenderer;
 
-    beforeEach(function() {
+    beforeEach(function () {
         renderer = TestUtils.createRenderer();
         renderer.render(<AuthForm />);
     });
 
-    it("should render correctly", function() {
+    it("should render correctly", function () {
         const result = renderer.getRenderOutput();
         expect(result.type).to.equal("div");
     });
 
-    describe("with signup", function() {
-        beforeEach(function() {
+    describe("with signup", function () {
+        beforeEach(function () {
             renderer = TestUtils.createRenderer();
-            renderer.render(<AuthForm signup={true}/>);
+            renderer.render(<AuthForm signup={true} />);
         });
-        it("should include the verify password input", function() {
+        it("should include the verify password input", function () {
             const result = renderer.getRenderOutput();
 
             for (let child of React.Children.toArray(result.props.children)) {
 
-              let childElement: React.ReactElement<any> = child as React.ReactElement<any>;
+                let childElement: React.ReactElement<any> = child as React.ReactElement<any>;
 
-              if (childElement.type === "form") {
-                  // The 'form' child should have three children
-                  expect(childElement.props.children.length).to.equal(3);
-              }
+                if (childElement.type === "form") {
+                    // The 'form' child should have three children
+                    expect(childElement.props.children.length).to.equal(3);
+                }
             }
         });
     });
