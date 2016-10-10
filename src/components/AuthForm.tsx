4632551086f00fa8efe959b6b56ae2cb8a341068
krisdan1011 <krisdan001011@gmail.com>
2016-10-10 11:43:32 -0400

    more terse conditional

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -17,16 +17,13 @@ export default class AuthForm extends React.Component<AuthFormProps, any> {
                     <input className="mdl-textfield__input" type="password" id="password" />
                     <label className="mdl-textfield__label" for="password">password</label>
                 </div>
-                {( () => {
-                  if (this.props.signup) {
-                  return (
+                { this.props.signup ? (
                     <div className="mdl-textfield mdl-js-textfield">
                         <input className="mdl-textfield__input" type="password" id="verifyPassword" />
                         <label className="mdl-textfield__label" for="password">verify password</label>
                     </div>
-                  );
+                  ) : null
                 }
-              })()}
             </form>
             <div className="mdl-card__actions mdl-card--border">
                 <button className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Log in</button>
