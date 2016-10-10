231a47167f42e15a17c10684f769602933350778
krisdan1011 <krisdan001011@gmail.com>
2016-10-09 21:33:21 -0400

    fixed warning for constructor

--- src/models/user.test.ts
+++ src/models/user.test.ts
@@ -2,7 +2,7 @@ import { User } from "./user";
 import { expect } from "chai";
 
 describe("User", function() {
-  describe("constructor", function() {
+  describe("default constructor", function() {
     it("should set the email", function() {
       let user = new User({email : "my@email.com"});
       expect(user.email).to.eq("my@email.com");
