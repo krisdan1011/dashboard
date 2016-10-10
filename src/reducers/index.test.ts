b34e4946015499976a923358fc38bccb51fe03ee
krisdan1011 <krisdan001011@gmail.com>
2016-10-10 16:12:51 -0400

    rename reducer test

--- src/reducers/index.test.ts
+++ src/reducers/index.test.ts
@@ -3,7 +3,7 @@ import { createStore } from "redux";
 import rootReducer from "./index";
 import { expect } from "chai";
 
-describe("rootReducer/session", function() {
+describe("Reducer ", function() {
   it("exists", () => {
     const store = createStore(rootReducer);
     const { session } = store.getState();
