0312b29f1d82148a043a5b6e5d6815dc8c4309d2
krisdan1011 <krisdan001011@gmail.com>
2016-10-13 18:50:14 -0400

    cell test

---
+++ src/components/Grid/Cell.test.tsx
@@ -0,0 +1,20 @@
+import { expect } from "chai";
+import * as React from "react";
+import * as TestUtils from "react-addons-test-utils";
+
+import Cell from "./Cell";
+
+describe("Cell", function() {
+  let renderer: React.ShallowRenderer;
+
+  beforeEach(() => {
+      renderer = TestUtils.createRenderer();
+      renderer.render(<Cell />);
+  });
+
+  it("should render correctly", function() {
+    const result = renderer.getRenderOutput();
+    expect(result.type).to.equal("div");
+    expect(result.props.className).to.contain("md-cell");
+  });
+});
