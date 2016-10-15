7294ff50955ae74a09416fafdb7420d115d9fdd4
krisdan1011 <krisdan001011@gmail.com>
2016-10-15 09:59:25 -0400

    authform tests

--- src/components/AuthForm.test.tsx
+++ src/components/AuthForm.test.tsx
@@ -1,24 +1,44 @@
-import { expect } from "chai";
-import * as React from "react";
-import * as TestUtils from "react-addons-test-utils";
+import * as chai from "chai";
+import { shallow } from "enzyme";
+// tslint:disable:no-unused-variable
+import * as React from "react"; // Needed for enzyme, unused for some reason.
+// tslint:enable:no-unused-variable
+import * as sinon from "sinon";
+import * as sinonChai from "sinon-chai";
 
 import AuthForm from "./AuthForm";
 
-describe("AuthForm", () => {
-    let renderer: React.ShallowRenderer;
-    let onSubmitAccessed = false;
-    let onSubmit = function() {
-        onSubmitAccessed = true;
-    };
+// Setup chai with sinon-chai
+chai.use(sinonChai);
+let expect = chai.expect;
 
-    beforeEach(function () {
-        onSubmitAccessed = false;
-        renderer = TestUtils.createRenderer();
-        renderer.render(<AuthForm onSubmit={ onSubmit } />);
+describe("AuthForm", function () {
+    it("renders the basics", function () {
+        const onSubmit = sinon.spy();
+        const wrapper = shallow(<AuthForm onSubmit={onSubmit} />);
+        // A form, two inputs and a button
+        expect(wrapper.find("form")).to.have.length(1);
+        expect(wrapper.find("input")).to.have.length(2);
+        expect(wrapper.find("button")).to.have.length(1);
     });
+    it("can be sumitted", function () {
+        const onSubmit = sinon.spy();
+        const wrapper = shallow(<AuthForm onSubmit={onSubmit} />);
 
-    it("should render correctly", function () {
-        const result = renderer.getRenderOutput();
-        expect(result.type).to.equal("div");
+        wrapper.find("button").simulate("click");
+        expect(onSubmit).to.have.been.called;
+    });
+    it("can be logged in with Github", function () {
+        const onSubmit = sinon.spy();
+        const onLoginWithGithub = sinon.spy();
+        const wrapper = shallow(
+            <AuthForm
+                onSubmit={onSubmit}
+                onLoginWithGithub={onLoginWithGithub} />
+        );
+
+        wrapper.find("button").last().simulate("click");
+        expect(onLoginWithGithub).to.have.been.called;
+        expect(onSubmit).to.have.not.been.called;
     });
 });
