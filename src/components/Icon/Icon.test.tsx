5c4492a3ee19349e23c5f535e381ae85ba91d00f
krisdan1011 <krisdan001011@gmail.com>
2016-10-18 14:19:56 -0400

    ICON tests

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -4,14 +4,27 @@ import { shallow } from "enzyme";
 import * as React from "react"; // Needed for enzyme, unused for some reason.
 // tslint:enable:no-unused-variable
 
-import ICON from "./constants";
-import Icon from "./Icon";
+import { Icon, ICON } from "./index";
 
 let expect = chai.expect;
 
-describe("Icon", function() {
-    it("renders correctly", function() {
-        const wrapper = shallow(<Icon width={ 200 } height={ 200 } icon={ ICON.GITHUB } />);
+describe("Icon", function () {
+    it("renders correctly", function () {
+        const wrapper = shallow(<Icon width={200} height={200} icon={ICON.GITHUB} />);
         expect(wrapper.find("svg")).to.have.length(1);
     });
 });
+
+describe("ICON", function () {
+    describe("GITHUB", function () {
+        it("exists", function () {
+            expect(ICON.GITHUB.path).to.not.be.null;
+        });
+    });
+    describe("DEFAULT_AVATAR", function () {
+        it("exists", function () {
+            expect(ICON.DEFAULT_AVATAR.path).to.not.be.null;
+        });
+    });
+});
+
