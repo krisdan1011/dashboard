a7a14361fc16241e6c6349a90b30ad50448af506
krisdan1011 <krisdan001011@gmail.com>
2016-10-19 11:35:26 -0400

    pass the classes through

--- src/frames/Login.tsx
+++ src/frames/Login.tsx
@@ -1,13 +1,20 @@
+import * as classNames from "classnames";
 import * as React from "react";
 
 import Content from "../components/Content";
 import Layout from "../components/Layout";
+import { CLASSES } from "../constants";
 
 export default class Login extends React.Component<any, any> {
+
+    classes() {
+        return classNames(this.props.classNames, CLASSES.COLOR.GREY_100);
+    }
+
     render() {
         return (
             <Layout>
-                <Content>
+                <Content classNames={ this.classes() } >
                     {this.props.children}
                 </Content>
             </Layout>
