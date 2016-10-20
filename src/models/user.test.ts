898a2eaf590cb22b65a2012ee42cf28c91f99347
krisdan1011 <krisdan001011@gmail.com>
2016-10-20 16:19:32 -0400

    no default constructor

--- src/models/user.test.ts
+++ src/models/user.test.ts
@@ -2,7 +2,7 @@ import  User  from "./user";
 import { expect } from "chai";
 
 describe("User", function() {
-  describe("default constructor", function() {
+  describe("constructor", function() {
     it("should set the email and token", function() {
       let user = new User({email : "my@email.com"});
       expect(user.email).to.eq("my@email.com");
