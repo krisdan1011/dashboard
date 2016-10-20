26f8526d35b407ba9b0b767dd8c4b6752480acae
krisdan1011 <krisdan001011@gmail.com>
2016-10-20 10:48:08 -0400

    formatting the menu file

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -18,15 +18,15 @@ export default class Menu extends MDLComponent<MenuProps, any> {
 
   classes() {
     return classNames("mdl-menu mdl-js-menu", {
-            [`mdl-menu--${this.props.valign}-${this.props.align}`]: true,
-            "mdl-js-ripple-effect": this.props.ripple
-        }, this.props.className);
+      [`mdl-menu--${this.props.valign}-${this.props.align}`]: true,
+      "mdl-js-ripple-effect": this.props.ripple
+    }, this.props.className);
   }
   render() {
     return (
-        <ul className={ this.classes() } htmlFor={this.props.target}>
-          { this.props.children }
-        </ul>
-        );
+      <ul className={this.classes()} htmlFor={this.props.target}>
+        {this.props.children}
+      </ul>
+    );
   }
 }
