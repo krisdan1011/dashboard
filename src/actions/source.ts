b0573c1b9fb192b98bc32825a3a18381d0124fc5
krisdan1011 <krisdan001011@gmail.com>
2016-11-03 14:56:10 -0400

    fixed bug where no sources exist

--- src/actions/source.ts
+++ src/actions/source.ts
@@ -21,20 +21,24 @@ export function setSources(sources: Source[]): SetSources {
 
 export function getSources(): Redux.ThunkAction<any, any, any> {
     return function (dispatch: Redux.Dispatch<any>) {
-        service.getSources().then(function(retVal) {
-            let keys = Object.keys(retVal.val());
-            let sources: Source[] = [];
+        service.getSources().then(function (retVal) {
+            if (!retVal.val()) {
+                dispatch(setSources([]));
+            } else {
+                let keys = Object.keys(retVal.val());
+                let sources: Source[] = [];
 
-            for (let key of keys) {
-                service.getSource(key).then(function(data) {
-                    let newSource = new Source(data.val());
-                    sources.push(newSource);
-                    // Dispatch the array of sources, note the .slice() is to make
-                    // a copy of the sources array.  This is needed otherwise redux
-                    // will not recognize it is a new array coming and not update
-                    // the components
-                    dispatch(setSources(sources.slice()));
-                });
+                for (let key of keys) {
+                    service.getSource(key).then(function (data) {
+                        let newSource = new Source(data.val());
+                        sources.push(newSource);
+                        // Dispatch the array of sources, note the .slice() is to make
+                        // a copy of the sources array.  This is needed otherwise redux
+                        // will not recognize it is a new array coming and not update
+                        // the components
+                        dispatch(setSources(sources.slice()));
+                    });
+                }
             }
         });
     };
