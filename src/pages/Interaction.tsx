6d25c2ea8db0795b91a3f094fec9155de266dd4f
krisdan1011 <krisdan001011@gmail.com>
2016-11-11 13:41:48 -0500

    Separating concerns

---
+++ src/pages/Interaction.tsx
@@ -0,0 +1,94 @@
+import Log from "../models/log";
+import * as React from "react";
+import JSONTree from "react-json-tree";
+
+interface InteractionProps {
+    request: Log;
+    response: Log;
+    theme?: any;
+
+}
+
+export class Interaction extends React.Component<InteractionProps, any> {
+    /**
+     * JSONTree uses base16 style
+     * For more info read http://chriskempson.com/projects/base16/
+     * and themes http://chriskempson.github.io/base16/
+     */
+    monokaiTheme = {
+        scheme: "monokai",
+        author: "wimer hazenberg (http://www.monokai.nl)",
+        base00: "#272822",
+        base01: "#383830",
+        base02: "#49483e",
+        base03: "#75715e",
+        base04: "#a59f85",
+        base05: "#f8f8f2",
+        base06: "#f5f4f1",
+        base07: "#f9f8f5",
+        base08: "#f92672",
+        base09: "#fd971f",
+        base0A: "#f4bf75",
+        base0B: "#a6e22e",
+        base0C: "#a1efe4",
+        base0D: "#66d9ef",
+        base0E: "#ae81ff",
+        base0F: "#cc6633"
+    };
+
+    getJSONTreeStyle() {
+        return {
+            padding: "15px",
+            borderRadius: "10px",
+            borderStyle: "solid",
+            borderWidth: "2px",
+            borderColor: this.monokaiTheme.base0B
+        };
+    }
+
+    getTheme() {
+        return this.props.theme ?
+            this.props.theme :
+            {
+                extend: this.monokaiTheme,
+                tree: this.getJSONTreeStyle()
+            };
+    }
+
+    shouldExpandNode(keyName: string[], data: any, level: number) {
+        // don't expand the really long nodes by default
+        if (keyName.indexOf("user") > -1 || keyName.indexOf("application") > -1) {
+            return false;
+        }
+        return true;
+    }
+
+    getTree(log: Log): JSX.Element {
+        let tree: JSX.Element;
+        if (log) {
+            tree = (
+                <JSONTree
+                    data={log.payload}
+                    hideRoot={true}
+                    invertTheme={false}
+                    theme={this.getTheme()}
+                    shouldExpandNode={this.shouldExpandNode} />
+            );
+        } else {
+            tree = (<p>Select a conversation or message to inspect the payload</p>);
+        }
+
+        return tree;
+    }
+
+    render() {
+        return (<div>
+                    <h6>REQUEST</h6>
+                        {this.getTree(this.props.request)}
+                    <h6>RESPONSE</h6>
+                        {this.getTree(this.props.response)}
+                </div>);
+    }
+}
+
+export default Interaction;
--- src/pages/LogsPage.tsx
+++ src/pages/LogsPage.tsx
@@ -9,6 +9,7 @@ import { Cell, Grid } from "../components/Grid";
 import Log from "../models/log";
 import Source from "../models/source";
 import { State } from "../reducers";
+import Interaction from "./Interaction";
 
 interface LogsPageProps {
     logs: Log[];
@@ -43,49 +44,6 @@ function mapDispatchToProps(dispatch: Redux.Dispatch<any>) {
 }
 
 export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
-
-    /**
-     * JSONTree uses base16 style
-     * For more info read http://chriskempson.com/projects/base16/
-     * and themes http://chriskempson.github.io/base16/
-     */
-    monokaiTheme = {
-        scheme: "monokai",
-        author: "wimer hazenberg (http://www.monokai.nl)",
-        base00: "#272822",
-        base01: "#383830",
-        base02: "#49483e",
-        base03: "#75715e",
-        base04: "#a59f85",
-        base05: "#f8f8f2",
-        base06: "#f5f4f1",
-        base07: "#f9f8f5",
-        base08: "#f92672",
-        base09: "#fd971f",
-        base0A: "#f4bf75",
-        base0B: "#a6e22e",
-        base0C: "#a1efe4",
-        base0D: "#66d9ef",
-        base0E: "#ae81ff",
-        base0F: "#cc6633"
-    };
-
-    getJSONTreeStyle() {
-        return {
-            padding: "15px",
-            borderRadius: "10px",
-            borderStyle: "solid",
-            borderWidth: "2px",
-            borderColor: this.monokaiTheme.base0B
-        };
-    }
-
-    getTheme() {
-        return {
-            extend: this.monokaiTheme,
-            tree: this.getJSONTreeStyle()
-        };
-    }
 
     constructor(props: LogsPageProps) {
         super(props);
@@ -157,14 +115,6 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
         });
     }
 
-    shouldExpandNode(keyName: string[], data: any, level: number) {
-        // don't expand the really long nodes by default
-        if (keyName.indexOf("user") > -1 || keyName.indexOf("application") > -1) {
-            return false;
-        }
-        return true;
-    }
-
     getContentHeight() {
         if (document.getElementsByClassName !== undefined) {
             let mains = document.getElementsByClassName("mdl-layout__content");
@@ -176,24 +126,6 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
         // Return default height, this is when the page isn't fully rendered
         //  or when we are unit testing
         return 200;
-    }
-
-    getTree(log: Log): JSX.Element {
-        let tree: JSX.Element;
-        if (log) {
-            tree = (
-                <JSONTree
-                    data={log.payload}
-                    hideRoot={true}
-                    invertTheme={false}
-                    theme={this.getTheme()}
-                    shouldExpandNode={this.shouldExpandNode} />
-            );
-        } else {
-            tree = (<p>Select a conversation or message to inspect the payload</p>);
-        }
-
-        return tree;
     }
 
     render() {
@@ -215,10 +147,9 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
                     */ }
                 </Cell>
                 <Cell col={6} style={{ maxHeight: this.getContentHeight() - 30, overflowY: "scroll" }}>
-                    <h6>REQUEST</h6>
-                    {this.getTree(this.state.request)}
-                    <h6>RESPONSE</h6>
-                    {this.getTree(this.state.response)}
+                    <Interaction
+                        request={this.state.request}
+                        response={this.state.response}/>
                 </Cell>
             </Grid>
         );
