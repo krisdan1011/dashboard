a706909441cb9eaa96e1f941b143fff80415cab3
krisdan1011 <krisdan001011@gmail.com>
2016-11-11 09:50:40 -0500

    test

--- src/reducers/index.test.ts
+++ src/reducers/index.test.ts
@@ -1,11 +1,13 @@
 import { expect } from "chai";
 import { createStore } from "redux";
 
+import { setCurrentSource } from "../actions/source";
+import { LOGOUT_USER } from "../constants";
+import Source from "../models/source";
 import rootReducer from "./index";
 
 /**
  * rootReducer Testing help outlined here: https://github.com/reactjs/redux/issues/1412#issuecomment-184696008
- *
  */
 describe("rootReducer", function() {
     it("returns the initial state", function() {
@@ -15,5 +17,18 @@ describe("rootReducer", function() {
         expect(store.getState().source).to.exist;
         expect(store.getState().log).to.exist;
         expect(store.getState().routing).to.exist;
+    });
+    it("resets on logout", function() {
+        let store = createStore(rootReducer);
+
+        // populate the store with some data
+        let source = new Source({name: "Source"});
+        store.dispatch(setCurrentSource(source));
+        expect(store.getState().source.currentSource).to.equal(source);
+
+        // send the logout action
+        store.dispatch({type: LOGOUT_USER});
+        // and ensure the data is cleared
+        expect(store.getState().source.currentSource).to.be.undefined;
     });
 });
