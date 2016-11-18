3e0fbbb6ca025771111af82f514d1f66cf6f458b
krisdan1011 <krisdan001011@gmail.com>
2016-11-18 18:05:35 -0500

    Removign comments.

--- src/services/auth.test.ts
+++ src/services/auth.test.ts
@@ -315,20 +315,4 @@ describe("Auth ts not mocked", function () {
             }, authService, localStorage);
         });
     });
-
-    // describe("sign up bad email", function() {
-    //     it ("Test the sign up funct.", function() {
-    //            auth.signUpWithEmail("testuser", "secretPassword", "secretPassword", (success: true, error: "") => void{});
-    //     });
-    // });
-    // describe("sign up short passes", function() {
-    //     it ("Test the sign up funct.", function() {
-    //            auth.signUpWithEmail("testuser@testuser.com", "secr", "secr", (success: true, error: "") => void{});
-    //     });
-    // });
-    // describe("sign up not matching passes", function() {
-    //     it ("Test the sign up funct.", function() {
-    //            auth.signUpWithEmail("testuser@testuser.com", "secradad", "secr", (success: true, error: "") => void{});
-    //     });
-    // });
 });
