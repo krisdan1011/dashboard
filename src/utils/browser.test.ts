a1316fdd0993242e1590d87c93ac54896b8a8a70
krisdan1011 <krisdan001011@gmail.com>
2016-11-21 14:15:06 -0500

    browser tests

--- src/utils/browser.test.ts
+++ src/utils/browser.test.ts
@@ -30,12 +30,40 @@ describe("Utils", function () {
             expect(browser.isMobileWidth).to.throw(ReferenceError);
         });
         it("returns true on small windows", function() {
-            let dummyWindow = {innerWidth: 479};
+            let dummyWindow = {innerWidth: (browser.mobileWidthThreshold - 1)};
             expect(browser.isMobileWidth(<Window>dummyWindow)).to.equal(true);
         });
         it("returns false on large windows", function() {
-            let dummyWindow = {innerWidth: 480};
+            let dummyWindow = {innerWidth: browser.mobileWidthThreshold};
             expect(browser.isMobileWidth(<Window>dummyWindow)).to.equal(false);
+        });
+    });
+    describe("onResize", function() {
+        it("thows an error on node.js if you don't pass in a window", function() {
+            expect(browser.onResize).to.throw(ReferenceError);
+        });
+        it("sets up an event listener for the 'resize` event", function() {
+            let registeredEvent: string;
+            let registeredCallback: (event: UIEvent) => void;
+            let dummyWindow = { addEventListener: function(event: string, callback: (event: UIEvent) => void) {
+                registeredEvent = event;
+                registeredCallback = callback;
+            }};
+            let callback = function(event: UIEvent) { };
+
+            browser.onResize(callback, <Window>dummyWindow);
+
+            expect(registeredEvent).to.equal("resize");
+            expect(registeredCallback).to.equal(callback);
+        });
+    });
+    describe("size", function() {
+        it("thows an error on node.js if you don't pass in a window", function() {
+            expect(browser.size).to.throw(ReferenceError);
+        });
+        it("returns the correct size", function() {
+            let dummyWindow = {innerWidth: 480, innerHeight: 1020};
+            expect(browser.size(<Window>dummyWindow)).to.deep.equal({width: 480, height: 1020});
         });
     });
 });
