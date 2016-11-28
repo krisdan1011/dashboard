a849ffa498121570158f0b42e5d6b812b3c7b329
krisdan1011 <krisdan001011@gmail.com>
2016-11-28 15:20:38 -0500

    Adding a compensation for rednering height in logs

--- src/pages/LogsPage.tsx
+++ src/pages/LogsPage.tsx
@@ -13,6 +13,16 @@ import Source from "../models/source";
 import { State } from "../reducers";
 import browser from "../utils/browser";
 
+interface CellDimensions {
+    height: number;
+}
+
+interface Dimensions {
+    width: number,
+    height: number,
+    cellDimens: CellDimensions
+}
+
 export interface LogsPageProps {
     logs: Log[];
     source: Source;
@@ -20,9 +30,8 @@ export interface LogsPageProps {
     params?: any;
 }
 
-export interface LogsPageState {
-    width: number;
-    height: number;
+interface LogsPageState {
+    lastDimens: Dimensions;
     source: Source | undefined;
     request: Log | undefined;
     response: Log | undefined;
@@ -51,8 +60,7 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
     constructor(props: LogsPageProps) {
         super(props);
         this.state = {
-            width: 0,
-            height: 0,
+            lastDimens: { width: 0, height: 0, cellDimens: { height: 0 } },
             source: props.source,
             request: undefined,
             response: undefined,
@@ -76,24 +84,43 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
     updateDimensions() {
         // Algorithm taken from https://andylangton.co.uk/blog/development/get-viewportwindow-size-width-and-height-javascript
         // Modified to get around unit tests which don't have half this.
-        let width: number, height: number;
+        let width: number, height: number, heightOffset: number;
         if (window) {
             let w = window,
                 d = document,
                 dElement = d.documentElement,
                 body = d.getElementsByClassName(Grid.GRID_CLASS)[0];
 
-                width = w.innerWidth || dElement.clientWidth || (body ? body.clientWidth : 0);
-                height = w.innerHeight || dElement.clientHeight || (body ? body.clientHeight : 0);
+            width = w.innerWidth || dElement.clientWidth || (body ? body.clientWidth : 0);
+            height = w.innerHeight || dElement.clientHeight || (body ? body.clientHeight : 0);
+
+            if (body) {
+                let rect = dElement.getBoundingClientRect();
+                let rect2 = body.getBoundingClientRect();
+                console.info("LIST: Offset = " + body.clientLeft + " " + body.clientTop + " " + body.clientWidth + " " + body.clientHeight);
+                console.info("LIST: rect2 " + rect2.left + " " + rect2.top + " " + rect2.right + " " + rect2.bottom);
+                console.info("LIST: rect2 Dimen " + rect2.width + " " + rect2.height);
+                heightOffset = rect2.top;
+            } else {
+                heightOffset = 0;
+            }
         } else {
             // Unit tests
             width = 200;
             height = 200;
+            heightOffset = 0;
         }
 
+        console.info("Final " + width + " " + height + " " + heightOffset);
+
         this.setState({
-            width: width,
-            height: height,
+            lastDimens: {
+                width: width,
+                height: height,
+                cellDimens: {
+                    height: height - heightOffset,
+                }
+            },
             source: (this.state) ? this.state.source : undefined,
             request: (this.state) ? this.state.request : undefined,
             response: (this.state) ? this.state.response : undefined,
@@ -106,8 +133,7 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
             this.props.getLogs(nextProps.source.secretKey);
             this.setState({
                 source: nextProps.source,
-                width: this.state.width,
-                height: this.state.height,
+                lastDimens: this.state.lastDimens,
                 request: this.state.request,
                 response: this.state.response,
                 outputs: this.state.outputs
@@ -117,28 +143,28 @@ export class LogsPage extends React.Component<LogsPageProps, LogsPageState> {
 
     onConversationClicked(conversation: Conversation, event: React.MouseEvent) {
         this.setState({
-            width: this.state.width,
-            height: this.state.height,
-            source: this.state.source,
             request: conversation.request,
             response: conversation.response,
-            outputs: conversation.outputs
+            outputs: conversation.outputs,
+            lastDimens: this.state.lastDimens,
+            source: this.state.source,
         });
     }
 
     render() {
+        console.info("RENDER " + this.state.lastDimens.width + " " + this.state.lastDimens.height + " " + this.state.lastDimens.cellDimens.height);
         return (
             <Grid
                 noSpacing={true}>
                 <Cell col={6} phone={4} tablet={4} style={{ paddingLeft: "10px", paddingRight: "5px" }}>
-                    <div style={{ maxHeight: this.state.height, overflowY: "auto" }}>
+                    <div style={{ maxHeight: this.state.lastDimens.cellDimens.height, overflowY: "auto" }}>
                         <ConversationListView
                             conversations={ConversationList.fromLogs(this.props.logs)}
                             expandListItemWhenActive={browser.isMobileWidth()}
                             onClick={this.onConversationClicked.bind(this)} />
                     </div>
                 </Cell>
-                <Cell col={6} hidePhone={true} tablet={4} style={{ maxHeight: this.state.height, overflowY: "scroll", paddingLeft: "5px", paddingRight: "10px" }}>
+                <Cell col={6} hidePhone={true} tablet={4} style={{ maxHeight: this.state.lastDimens.cellDimens.height, overflowY: "scroll", paddingLeft: "5px", paddingRight: "10px" }}>
                     {this.state.request ?
                         (
                             <Interaction
