b1b5db4c5d9361935750a9e703bbac664b772982
krisdan1011 <krisdan001011@gmail.com>
2016-12-07 11:13:05 -0500

    Adding unit test for the unregister event

--- src/utils/browser.test.ts
+++ src/utils/browser.test.ts
@@ -25,40 +25,67 @@ describe("Utils", function () {
             expect(browser.isMobileOrTabletImpl("", "")).to.be.false;
         });
     });
-    describe("isMobileWidth", function() {
-        it("throws an error on node.js if you don't pass in a window", function() {
+    describe("isMobileWidth", function () {
+        it("throws an error on node.js if you don't pass in a window", function () {
             expect(browser.isMobileWidth).to.throw(ReferenceError);
         });
-        it("returns true on small windows", function() {
-            let dummyWindow = {innerWidth: (browser.mobileWidthThreshold - 1)};
+        it("returns true on small windows", function () {
+            let dummyWindow = { innerWidth: (browser.mobileWidthThreshold - 1) };
             expect(browser.isMobileWidth(<Window>dummyWindow)).to.equal(true);
         });
-        it("returns false on large windows", function() {
-            let dummyWindow = {innerWidth: browser.mobileWidthThreshold};
+        it("returns false on large windows", function () {
+            let dummyWindow = { innerWidth: browser.mobileWidthThreshold };
             expect(browser.isMobileWidth(<Window>dummyWindow)).to.equal(false);
         });
     });
-    describe("onResize", function() {
-        it("sets up an event listener for the 'resize` event", function() {
+    describe("onResize", function () {
+        it("sets up an event listener for the 'resize` event", function () {
             let registeredEvent: string;
             let registeredCallback: (event: UIEvent) => void;
-            let dummyWindow = { addEventListener: function(event: string, callback: (event: UIEvent) => void) {
-                registeredEvent = event;
-                registeredCallback = callback;
-            }};
-            let callback = function(event: UIEvent) { };
+            let dummyWindow = {
+                addEventListener: function (event: string, callback: (event: UIEvent) => void) {
+                    registeredEvent = event;
+                    registeredCallback = callback;
+                }
+            };
+            let callback = function (event: UIEvent) { };
 
             browser.onResize(callback, <Window>dummyWindow).register();
 
             expect(registeredEvent).to.equal("resize");
             expect(registeredCallback).to.equal(callback);
         });
+
+        it("sets up an event listener for the `resize` event then tests the unregister method", function () {
+            let registeredEvent: string;
+            let registeredCallback: (event: UIEvent) => void;
+            let unregisteredEvent: string;
+            let unregisteredCallback: (event: UIEvent) => void;
+            let dummyWindow = {
+                addEventListener: function (event: string, callback: (event: UIEvent) => void) {
+                    registeredEvent = event;
+                    registeredCallback = callback;
+                },
+
+                removeEventListener: function(event: string, callback: (event: UIEvent) => void) {
+                    unregisteredEvent = event;
+                    unregisteredCallback = callback;
+                }
+            };
+            let callback = function (event: UIEvent) { };
+
+            browser.onResize(callback, <Window>dummyWindow).register();
+            browser.onResize(callback, <Window>dummyWindow).unregister();
+
+            expect(registeredEvent).to.equal("resize");
+            expect(unregisteredCallback).to.equal(callback);
+        });
     });
 
-    describe("size", function() {
-        it("returns the correct size", function() {
-            let dummyWindow = {innerWidth: 480, innerHeight: 1020};
-            expect(browser.size(<Window>dummyWindow)).to.deep.equal({width: 480, height: 1020});
+    describe("size", function () {
+        it("returns the correct size", function () {
+            let dummyWindow = { innerWidth: 480, innerHeight: 1020 };
+            expect(browser.size(<Window>dummyWindow)).to.deep.equal({ width: 480, height: 1020 });
         });
     });
 });
