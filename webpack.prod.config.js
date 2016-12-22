91add275bfe381fc8c74332c90020fe9b4837248
krisdan1011 <krisdan001011@gmail.com>
2016-12-21 20:07:07 -0500

    fingers crossed!

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -4,6 +4,11 @@ var config = require("./webpack.config");
 var webpack = require("webpack");
 
 config.output.filename = "scripts/dashboard.min.js";
+config.plugins.push(new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+}));
 config.plugins.push(new webpack.optimize.UglifyJsPlugin());
 
 module.exports = config;
