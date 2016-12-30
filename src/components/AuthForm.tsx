a1840cbcc25dc2e62c7d1585125b4e5d5205e1f7
krisdan1011 <krisdan001011@gmail.com>
2016-12-30 17:58:43 -0500

    New Authform look

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 
+import { Button } from 'react-toolbox/lib/button';
+
 import FormInput from "./FormInput";
 import { Icon, ICON } from "./Icon";
+
+import theme = require("../themes/authform.scss");
 
 export interface AuthFormProps {
     email?: string;
@@ -33,70 +37,65 @@ export default class AuthForm extends React.Component<AuthFormProps, AuthFormSta
     render() {
         return (
             <div className="mdl-card__supporting-text">
-                <form id="auth" onSubmit={this.props.onSubmit} >
-                    <FormInput
-                        label={"Email"}
-                        type={"text"}
-                        floatingLabel={true}
-                        value={this.props.email}
-                        onChange={this.props.onChange} />
-                    <FormInput
-                        label={"Password"}
-                        type={"password"}
-                        floatingLabel={true}
-                        value={this.props.password}
-                        onChange={this.props.onChange} />
-                    <FormInput
-                        label={"Confirm Password"}
-                        type={"password"}
-                        floatingLabel={true}
-                        value={this.props.confirmPassword}
-                        onChange={this.props.onChange}
-                        hidden={!this.state.isConfirmPasswordVisible} />
-                    <div style={{ color: "#d50000", marginTop: "5px", marginBottom: "10px" }}>
-                        <label>{this.props.error}</label>
+                    {this.props.onLoginWithGithub ? (
+                    <div style={{ width: "100%", textAlign: "Center" }}>
+                        <Button
+                            theme={theme}
+                            label="Login with Github"
+                            icon={(<Icon style={{ marginRight: "13px" }} width={20} height={20} icon={ICON.GITHUB} />)}
+                            onClick={this.props.onLoginWithGithub.bind(this)} />
                     </div>
-                </form>
-                <div className="mdl-card__actions mdl-card--border clearfix">
-                    <input
-                        type="submit"
-                        form="auth"
-                        value="Login"
-                        className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-pull-left"
-                        />
+                    ) : undefined}
+                <div style={{ marginTop: "16px" }} className="mdl-card__actions mdl-card--border clearfix">
+                    <form id="auth" onSubmit={this.props.onSubmit} >
+                        <FormInput
+                            label={"Email"}
+                            type={"text"}
+                            floatingLabel={true}
+                            value={this.props.email}
+                            onChange={this.props.onChange} />
+                        <FormInput
+                            label={"Password"}
+                            type={"password"}
+                            floatingLabel={true}
+                            value={this.props.password}
+                            onChange={this.props.onChange} />
+                        <FormInput
+                            label={"Confirm Password"}
+                            type={"password"}
+                            floatingLabel={true}
+                            value={this.props.confirmPassword}
+                            onChange={this.props.onChange}
+                            hidden={!this.state.isConfirmPasswordVisible} />
+                        <div style={{ color: "#d50000", marginTop: "5px", marginBottom: "10px" }}>
+                            <label>{this.props.error}</label>
+                        </div>
+                    </form>
+                    <Button
+                        theme={theme}
+                        label="Login"
+                        onClick={this.props.onSignUpWithEmail.bind(this)} />
                     {!this.state.isConfirmPasswordVisible ? (
-                        <button
-                            className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-pull-right"
-                            onClick={this.onRegister.bind(this)}>
-                            Sign Up
-                        </button>
+                        <Button
+                            theme={theme}
+                            label="Sign Up"
+                            onClick={this.props.onSignUpWithEmail.bind(this)} />
                     ) : undefined}
                     {this.state.isConfirmPasswordVisible ? (
-                        <button
-                            className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-pull-right"
-                            onClick={this.props.onSignUpWithEmail}>
-                            Submit
-                        </button>
+                        <Button
+                            theme={theme}
+                            label="Submit"
+                            onClick={this.props.onSignUpWithEmail.bind(this)} />
                     ) : undefined}
                 </div>
                 <div className="mdl-card__actions clearfix">
                     <button
-                    onClick={this.props.onResetPassword}
-                    className="mdl-button mdl-js-button mdl-js-ripple-effect"
-                    style={{height: "16px", fontSize: "12px", color: "#03A9F4", textTransform: "none", lineHeight: "12px"}}>
+                        onClick={this.props.onResetPassword}
+                        className="mdl-button mdl-js-button mdl-js-ripple-effect"
+                        style={{ float: "right", height: "16px", fontSize: "12px", color: "#03A9F4", textTransform: "none", lineHeight: "12px" }}>
                         Reset Password
                     </button>
                 </div>
-                {this.props.onLoginWithGithub ? (
-                    <div className="mdl-card__actions mdl-card--border">
-                        <button
-                            className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"
-                            onClick={this.props.onLoginWithGithub}>
-                            <Icon style={{ marginRight: "13px" }} width={20} height={20} icon={ICON.GITHUB} />
-                            Login with Github
-                        </button>
-                    </div>
-                ) : undefined}
             </div>
         );
     }
