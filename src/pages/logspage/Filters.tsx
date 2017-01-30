e811e67aeb07b00a73d34f8da952f0a75568d112
krisdan1011 <krisdan001011@gmail.com>
2017-01-30 17:46:13 -0500

    Removing console

--- src/pages/logspage/Filters.tsx
+++ src/pages/logspage/Filters.tsx
@@ -168,19 +168,15 @@ export class ExceptionFilter implements FilterType {
     type: string = TYPE_EXCEPTION;
 
     constructor(hasException?: boolean) {
-        console.info("Constructor " + hasException);
         this.hasException = (hasException !== undefined) ? hasException : true;
     }
 
     get filter(): (item: Conversation) => boolean {
         const hasException = this.hasException;
         return function(item: Conversation): boolean {
-            console.info("Checking " + hasException);
             if (hasException) {
-                console.info("Returning from exception");
                 return item.hasException;
             } else {
-                console.info("Returning from no exception");
                 return !item.hasException;
             }
         };
