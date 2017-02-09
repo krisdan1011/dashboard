05cdcf379c54bcb00d8d17a5eaf6689d45d681c7
krisdan1011 <krisdan001011@gmail.com>
2017-02-09 17:55:26 -0500

    Adding test to log actions

--- src/actions/log.test.ts
+++ src/actions/log.test.ts
@@ -1,5 +1,4 @@
 import * as chai from "chai";
-import * as fetchMock from "fetch-mock";
 import * as sinon from "sinon";
 
 import configureMockStore, { IStore } from "redux-mock-store";
@@ -9,6 +8,7 @@ import { FETCH_LOGS_REQUEST, SET_LOGS } from "../constants";
 import Log from "../models/log";
 import LogQuery from "../models/log-query";
 import Source from "../models/source";
+import { LogQueryEvent } from "../reducers/log";
 import LogsService from "../services/log";
 import { dummyLogs } from "../utils/test";
 import * as log from "./log";
@@ -29,26 +29,7 @@ describe("Log Actions", function () {
         let store: IStore<any>;
 
         beforeEach(function () {
-            fetchMock.get("*", {
-                "data": mockPayload
-            });
-
             store = mockStore(initialState);
-        });
-
-        afterEach(function () {
-            fetchMock.restore();
-        });
-
-        it("retrieves the logs", function () {
-            return store.dispatch(log.getLogs(source)).then(function () {
-
-                let actions: any[] = store.getActions();
-
-                expect(actions).to.have.length(3);
-                expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
-                expect(actions[1].type).to.equal(SET_LOGS);
-            });
         });
 
         describe("RetrieveLogs", function () {
@@ -69,45 +50,158 @@ describe("Log Actions", function () {
                 });
             });
 
-            it("Tests that the proper dispatches were thrown on success.", function () {
-                return store.dispatch(log.retrieveLogs(query)).then(function (logs: Log[]) {
-                    let actions: any[] = store.getActions();
+            describe("Success", function () {
+                let serviceStub: Sinon.SinonStub;
 
-                    expect(actions).to.have.length(3); // Two fetching dispatches and set logs.
-                    expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
-                    expect(actions[0].fetching).to.equal(true);
+                before(function () {
+                    serviceStub = sinon.stub(LogsService, "getLogs").returns(new Promise((resolve, reject) => {
+                        resolve(mockPayload);
+                    }));
+                });
 
-                    expect(actions[1].type).to.equal(SET_LOGS);
-                    expect(actions[1].logs).to.have.length(logs.length);
-                    // expect(actions[1].logs).to.have.deep.equal(mockPayload);  // TODO: It can't compare the timestamp properly
-                    expect(actions[1].append).to.equal(false);
+                afterEach(function () {
+                    serviceStub.reset();
+                });
 
-                    expect(actions[2].type).to.equal(FETCH_LOGS_REQUEST);
-                    expect(actions[2].fetching).to.equal(false);
+                after(function () {
+                    serviceStub.restore();
+                });
+
+                it("retrieves the logs", function () {
+                    return store.dispatch(log.getLogs(source)).then(function (logs: Log[]) {
+                        expect(logs).to.deep.equal(mockPayload);
+
+                        let actions: any[] = store.getActions();
+
+                        expect(actions).to.have.length(3);
+                        expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[1].type).to.equal(SET_LOGS);
+                    });
+                });
+
+                it("Tests that the proper dispatches were thrown on success.", function () {
+                    return store.dispatch(log.retrieveLogs(query)).then(function (logs: Log[]) {
+                        let actions: any[] = store.getActions();
+
+                        expect(actions).to.have.length(3); // Two fetching dispatches and set logs.
+                        expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[0].fetching).to.equal(true);
+
+                        expect(actions[1].type).to.equal(SET_LOGS);
+                        expect(actions[1].logs).to.deep.equal(logs);
+                        expect(actions[1].append).to.equal(false);
+
+                        expect(actions[2].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[2].fetching).to.equal(false);
+                    });
                 });
             });
 
-            it("Tests that the proper dispatches were thrown on failure.", function () {
+            describe("Failure", function () {
 
-                const serviceStub = sinon.stub(LogsService, "getLogs").returns(new Promise((resolve, reject) => {
-                    reject(new Error("Error thrown per requirements of the test."));
-                }));
+                let serviceStub: Sinon.SinonStub;
 
-                return store.dispatch(log.retrieveLogs(query)).catch(function (err: Error) {
-                    let actions: any[] = store.getActions();
+                before(function () {
+                    serviceStub = sinon.stub(LogsService, "getLogs").returns(new Promise((resolve, reject) => {
+                        reject(new Error("Error thrown per requirements of the test."));
+                    }));
+                });
 
-                    expect(err).to.not.be.undefined;
+                afterEach(function () {
+                    serviceStub.reset();
+                });
 
-                    expect(actions).to.have.length(2); // Two fetching dispatches and set logs.
-                    expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
-                    expect(actions[0].fetching).to.equal(true);
-
-                    expect(actions[1].type).to.equal(FETCH_LOGS_REQUEST);
-                    expect(actions[1].fetching).to.equal(false);
-                }).then(function() {
+                after(function () {
                     serviceStub.restore();
-                }).catch(function() {
+                });
+
+                it("Tests that the proper dispatches were thrown on failure.", function () {
+                    return store.dispatch(log.retrieveLogs(query)).catch(function (err: Error) {
+                        let actions: any[] = store.getActions();
+
+                        expect(err).to.not.be.undefined;
+
+                        expect(actions).to.have.length(2); // Two fetching dispatches and set logs.
+                        expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[0].fetching).to.equal(true);
+
+                        expect(actions[1].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[1].fetching).to.equal(false);
+                    });
+                });
+            });
+        });
+
+        describe("NextPage", function () {
+            let sevenDaysAgo: Date;
+            let today: Date;
+            let query: LogQuery;
+
+            before(function () {
+                sevenDaysAgo = new Date();
+                sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
+                today = new Date();
+
+                query = new LogQuery({
+                    source: source,
+                    startTime: sevenDaysAgo,
+                    endTime: today
+                });
+            });
+
+            describe("Success", function () {
+                let serviceStub: Sinon.SinonStub;
+
+                let originalQuery: LogQueryEvent;
+
+                let nextPage: Log[] = dummyLogs(5);
+
+                before(function () {
+                    serviceStub = sinon.stub(LogsService, "getLogs").returns(new Promise((resolve, reject) => {
+                        resolve(nextPage);
+                    }));
+
+                    originalQuery = {
+                        logs: mockPayload,
+                        query: new LogQuery({
+                            source: source,
+                            startTime: sevenDaysAgo,
+                            endTime: today
+                        })
+                    };
+                });
+
+                afterEach(function () {
+                    serviceStub.reset();
+                });
+
+                after(function () {
                     serviceStub.restore();
+                });
+
+                it("Checks the dispatches were shown.", function () {
+                    return store.dispatch(log.nextPage(originalQuery, 50)).then(function (logs: Log[]) {
+                        let actions: any[] = store.getActions();
+
+                        expect(actions).to.have.length(3); // Two fetching dispatches and set logs.
+                        expect(actions[0].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[0].fetching).to.equal(true);
+
+                        expect(actions[1].type).to.equal(SET_LOGS);
+                        expect(actions[1].logs).to.deep.equal(logs);
+                        expect(actions[1].query).to.deep.equal(originalQuery.query);
+                        expect(actions[1].append).to.equal(false);
+
+                        expect(actions[2].type).to.equal(FETCH_LOGS_REQUEST);
+                        expect(actions[2].fetching).to.equal(false);
+                    });
+                });
+
+                it("Checks the logs returned have the new pages appended to them.", function () {
+                    return store.dispatch(log.nextPage(originalQuery, 50)).then(function (logs: Log[]) {
+                        const merged = mockPayload.slice().concat(nextPage);
+                        expect(merged).to.deep.equal(logs);
+                    });
                 });
             });
         });
@@ -117,16 +211,28 @@ describe("Log Actions", function () {
             let initialState: any;
             let store: any;
             let source: Source;
+            let serviceStub: Sinon.SinonStub;
 
             before(function () {
                 source = new Source({
                     name: "Test"
                 });
+                serviceStub = sinon.stub(LogsService, "getLogs").returns(new Promise((resolve, reject) => {
+                    resolve(mockPayload);
+                }));
             });
 
             beforeEach(function () {
                 initialState = {};
                 store = mockStore(initialState);
+            });
+
+            afterEach(function() {
+                serviceStub.reset();
+            });
+
+            after(function () {
+                serviceStub.restore();
             });
 
             it("Sets the appropriate source for the query.", function () {
