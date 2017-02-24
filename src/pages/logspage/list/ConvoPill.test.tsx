1a3aff65f7d1e6685ad0d7933c93a7ca377f1c97
krisdan1011 <krisdan001011@gmail.com>
2017-02-24 16:43:44 -0500

    Adding ConvoPill tests

---
+++ src/pages/logspage/list/ConvoPill.test.tsx
@@ -0,0 +1,38 @@
+import * as chai from "chai";
+import { shallow, ShallowWrapper } from "enzyme";
+import * as React from "react";
+
+import Pill from "../../../components/Pill";
+import ConvoPill from "./ConvoPill";
+
+const expect = chai.expect;
+
+describe("ConvoPill", function () {
+    describe("Render", function () {
+        let wrapper: ShallowWrapper<any, any>;
+
+        describe("Visible", function () {
+            before(function () {
+                wrapper = shallow(<ConvoPill show={true} text={"Test"} />);
+            });
+
+            it("Tests that the pill is visible", function () {
+                expect(wrapper.find(Pill)).to.have.length(1);
+            });
+
+            it("Tests the pill has the text.", function () {
+                expect(wrapper.find(Pill)).to.have.text("Test");
+            });
+        });
+
+        describe("Invisible", function () {
+            before(function () {
+                wrapper = shallow(<ConvoPill show={false} text={"Test"} />);
+            });
+
+            it("Tests that the pill is invisible", function() {
+                expect(wrapper.find(Pill)).to.have.length(0);
+            });
+        });
+    });
+});
