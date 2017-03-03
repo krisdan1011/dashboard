d8750a87e3f3b0408a299e26c6d5319d22f23b97
krisdan1011 <krisdan001011@gmail.com>
2017-03-03 15:01:02 -0500

    adding styled menu test

--- src/components/Menu/StyledMenu.tsx
+++ src/components/Menu/StyledMenu.tsx
@@ -1,20 +1,16 @@
-import { Component } from "react";
-
+import * as React from "react";
 import { IconMenu, IconMenuProps } from "react-toolbox/lib/menu";
 
 const MenuTheme = require("../../themes/menu.scss");
 
-export interface StyledMenuProps extends IconMenuProps {
-}
+export interface StyledMenuProps extends IconMenuProps { }
 
-interface StyledMenuState {
-
-}
+interface StyledMenuState { }
 
 /**
  * This exists to re-use the consistent theme around the list.
  */
-class StyledMenu extends Component<StyledMenuProps, StyledMenuState> {
+class StyledMenu extends React.Component<StyledMenuProps, StyledMenuState> {
     render() {
         const {theme, ...others} = this.props;
 
