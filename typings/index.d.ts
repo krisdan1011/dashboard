192ea37b69addc2194938d4cc08374b849d21ff0
krisdan1011 <krisdan001011@gmail.com>
2017-04-11 17:02:54 -0400

    Moving classnames to @types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,5 @@
 /// <reference path="globals/chai-enzyme/index.d.ts" />
 /// <reference path="globals/chai/index.d.ts" />
-/// <reference path="globals/classnames/index.d.ts" />
 /// <reference path="globals/enzyme/index.d.ts" />
 /// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/fetch-mock/index.d.ts" />
