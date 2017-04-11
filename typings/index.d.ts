4108a36c812e6381ddab24eaa431285dd9ca4996
krisdan1011 <krisdan001011@gmail.com>
2017-04-11 17:05:21 -0400

    Moving es6-promise to @types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="globals/chai-enzyme/index.d.ts" />
 /// <reference path="globals/chai/index.d.ts" />
 /// <reference path="globals/enzyme/index.d.ts" />
-/// <reference path="globals/es6-promise/index.d.ts" />
 /// <reference path="globals/fetch-mock/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/object-assign/index.d.ts" />
