32c556fb3513c0c2c122fa11c8227315317b7abb
krisdan1011 <krisdan001011@gmail.com>
2017-04-11 16:41:46 -0400

    Moving bluebird to @types

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -28,7 +28,6 @@
 /// <reference path="globals/redux/index.d.ts" />
 /// <reference path="globals/sinon-chai/index.d.ts" />
 /// <reference path="globals/sinon/index.d.ts" />
-/// <reference path="modules/bluebird/index.d.ts" />
 /// <reference path="modules/chai-datetime/index.d.ts" />
 /// <reference path="modules/d3-scale/index.d.ts" />
 /// <reference path="modules/d3-time/index.d.ts" />
