d36d6b4f972f7d2080e2b1661799cee0443be60c
krisdan1011 <krisdan001011@gmail.com>
2017-08-04 11:59:51 -0500

    show empty graph (#406)
    * show empty graph
    
    * multiline wrapped on parenthesys
    
    * space after parenthesys
    
    * wrapp jsx elements

--- src/pages/sourcepage/SourceFullSummary.tsx
+++ src/pages/sourcepage/SourceFullSummary.tsx
@@ -21,159 +21,168 @@ type SelectedStatEntry = "stats" | "Amazon.Alexa" | "Google.Home" | "Unknown";
 
 // The entry corresponds with the label for easy pickens.
 type LabelMap<T> = {
-  [label: string]: T;
+    [label: string]: T;
 };
 
 interface SourceFullSummaryProps {
-  header: string;
-  source: Source;
-  startDate: moment.Moment;
-  endDate: moment.Moment;
+    header: string;
+    source: Source;
+    startDate: moment.Moment;
+    endDate: moment.Moment;
 }
 
 interface SourceFullSummaryState {
-  selectedStatEntry: SelectedStatEntry[];
-  sourceOptions: SourceOption[];
-  lines: LineProps[];
-  bars: BarProps[];
-  showUpTime?: boolean;
+    selectedStatEntry: SelectedStatEntry[];
+    sourceOptions: SourceOption[];
+    lines: LineProps[];
+    bars: BarProps[];
+    showUpTime?: boolean;
+    showEmptyGraph?: boolean;
 }
 
 function values<T>(obj: LabelMap<T>): T[] {
-  return Object.keys(obj).map(function (key) {
-    return obj[key];
-  });
+    return Object.keys(obj).map(function (key) {
+        return obj[key];
+    });
 }
 
 export class SourceFullSummary extends React.Component<SourceFullSummaryProps, SourceFullSummaryState> {
-  static options: LabelMap<SourceOption> = {
-    "Total": {
-      label: "Total",
-      theme: AllCheckboxTheme,
-      checked: true
-    },
-    "Amazon": {
-      label: "Amazon",
-      theme: AmazonCheckboxTheme,
-      checked: true
-    },
-    "Google": {
-      label: "Google",
-      theme: GoogleCheckboxTheme,
-      checked: true
-    }
-  };
-
-  static lines: LabelMap<LineProps> = {
-    "Total": {
-      dataKey: "total",
-      name: "Total",
-      stroke: BLACK
-    },
-    "Amazon": {
-      dataKey: "Amazon.Alexa",
-      name: "Alexa",
-      stroke: AMAZON_ORANGE
-    },
-    "Google": {
-      dataKey: "Google.Home",
-      name: "Action",
-      stroke: GOOGLE_GREEN
-    }
-  };
-
-  static bars: LabelMap<BarProps> = {
-    "Amazon": {
-      dataKey: "Amazon.Alexa",
-      name: "Alexa",
-      fill: AMAZON_ORANGE,
-      stackId: "a"
-    },
-    "Google": {
-      dataKey: "Google.Home",
-      name: "Action",
-      fill: GOOGLE_GREEN,
-      stackId: "a"
-    }
-  };
-
-  static statEntries: LabelMap<SelectedStatEntry> = {
-    "Total": "stats",
-    "Amazon": "Amazon.Alexa",
-    "Google": "Google.Home"
-  };
-
-  constructor(props: SourceFullSummaryProps) {
-    super(props);
-
-    this.handleOriginChange = this.handleOriginChange.bind(this);
-    this.handleShowUpTime = this.handleShowUpTime.bind(this);
-
-    this.state = {
-      sourceOptions: values(SourceFullSummary.options),
-      lines: values(SourceFullSummary.lines),
-      bars: values(SourceFullSummary.bars),
-      selectedStatEntry: [SourceFullSummary.statEntries["Total"]],
-      showUpTime: true
-    };
-  }
-
-  handleOriginChange(index: number, label: string, checked: boolean) {
-    let totalChecked: boolean = false;
-
-    this.state.sourceOptions[index].checked = checked;
-    this.state.lines = [];
-    this.state.bars = [];
-    this.state.selectedStatEntry = [];
-
-    for (let o of this.state.sourceOptions) {
-      if (o.checked) {
-        this.state.selectedStatEntry.push(SourceFullSummary.statEntries[o.label]);
-        this.state.lines.push(SourceFullSummary.lines[o.label]);
-        if (o.label !== "Total") {
-          this.state.bars.push(SourceFullSummary.bars[o.label]);
-        } else {
-          totalChecked = o.checked;
+    static options: LabelMap<SourceOption> = {
+        "Total": {
+            label: "Total",
+            theme: AllCheckboxTheme,
+            checked: true
+        },
+        "Amazon": {
+            label: "Amazon",
+            theme: AmazonCheckboxTheme,
+            checked: true
+        },
+        "Google": {
+            label: "Google",
+            theme: GoogleCheckboxTheme,
+            checked: true
         }
-      }
+    };
+
+    static lines: LabelMap<LineProps> = {
+        "Total": {
+            dataKey: "total",
+            name: "Total",
+            stroke: BLACK
+        },
+        "Amazon": {
+            dataKey: "Amazon.Alexa",
+            name: "Alexa",
+            stroke: AMAZON_ORANGE
+        },
+        "Google": {
+            dataKey: "Google.Home",
+            name: "Action",
+            stroke: GOOGLE_GREEN
+        }
+    };
+
+    static bars: LabelMap<BarProps> = {
+        "Amazon": {
+            dataKey: "Amazon.Alexa",
+            name: "Alexa",
+            fill: AMAZON_ORANGE,
+            stackId: "a"
+        },
+        "Google": {
+            dataKey: "Google.Home",
+            name: "Action",
+            fill: GOOGLE_GREEN,
+            stackId: "a"
+        }
+    };
+
+    static statEntries: LabelMap<SelectedStatEntry> = {
+        "Total": "stats",
+        "Amazon": "Amazon.Alexa",
+        "Google": "Google.Home"
+    };
+
+    constructor(props: SourceFullSummaryProps) {
+        super(props);
+
+        this.handleOriginChange = this.handleOriginChange.bind(this);
+        this.handleShowUpTime = this.handleShowUpTime.bind(this);
+        this.handleShowEmptyGraph = this.handleShowEmptyGraph.bind(this);
+
+        this.state = {
+            sourceOptions: values(SourceFullSummary.options),
+            lines: values(SourceFullSummary.lines),
+            bars: values(SourceFullSummary.bars),
+            selectedStatEntry: [SourceFullSummary.statEntries["Total"]],
+            showUpTime: true,
+            showEmptyGraph: true,
+        };
     }
 
-    // We don't care what's check for stat entry if "Total" is selected.
-    if (totalChecked) {
-      this.state.selectedStatEntry = [SourceFullSummary.statEntries["Total"]];
+    handleOriginChange(index: number, label: string, checked: boolean) {
+        let totalChecked: boolean = false;
+
+        this.state.sourceOptions[index].checked = checked;
+        this.state.lines = [];
+        this.state.bars = [];
+        this.state.selectedStatEntry = [];
+
+        for (let o of this.state.sourceOptions) {
+            if (o.checked) {
+                this.state.selectedStatEntry.push(SourceFullSummary.statEntries[o.label]);
+                this.state.lines.push(SourceFullSummary.lines[o.label]);
+                if (o.label !== "Total") {
+                    this.state.bars.push(SourceFullSummary.bars[o.label]);
+                } else {
+                    totalChecked = o.checked;
+                }
+            }
+        }
+
+        // We don't care what's check for stat entry if "Total" is selected.
+        if (totalChecked) {
+            this.state.selectedStatEntry = [SourceFullSummary.statEntries["Total"]];
+        }
+
+        this.setState(this.state);
     }
 
-    this.setState(this.state);
-  }
+    handleShowUpTime(showUpTime: boolean) {
+        this.state.showUpTime = showUpTime;
+        this.setState(this.state);
+    }
 
-  handleShowUpTime(showUpTime: boolean) {
-    this.state.showUpTime = showUpTime;
-    this.setState(this.state);
-  }
+    handleShowEmptyGraph(showGraph: boolean) {
+        this.state.showEmptyGraph = showGraph;
+        this.setState(this.state);
+    }
 
-  render() {
-    const {header, ...others} = this.props;
-    const {bars, lines, selectedStatEntry, showUpTime} = this.state;
-    const options = SourceFullSummary.options;
-    const handleOriginChange = this.handleOriginChange;
-    const handleShowUpTime = this.handleShowUpTime;
+    render() {
+        const {header, ...others} = this.props;
+        const {bars, lines, selectedStatEntry, showUpTime, showEmptyGraph} = this.state;
+        const options = SourceFullSummary.options;
+        const handleOriginChange = this.handleOriginChange;
+        const handleShowUpTime = this.handleShowUpTime;
+        const handleShowEmptyGraph = this.handleShowEmptyGraph;
 
-    return (
-      <div>
-        <Grid>
-          <h4 style={{paddingLeft: 45, margin: "8px 0 0"}}>{header}</h4>
-        </Grid>
-        <span>
+        return (
+            <div>
+                <Grid>
+                    <h4 style={{paddingLeft: 45, margin: "8px 0 0"}}>{header}</h4>
+                </Grid>
+                <span>
           <Grid className="custom-spacing" noSpacing={true}>
             <Cell className="origin-selector" tablet={8} col={6}>
               <SourceOriginSelector
-                options={values(options)}
-                onCheck={handleOriginChange}/>
+                  options={values(options)}
+                  onCheck={handleOriginChange}/>
             </Cell>
             <Cell className="stats" tablet={8} col={6}>
               <SourceStats
-                selectedEntries={selectedStatEntry}
-                {...others} />
+                  selectedEntries={selectedStatEntry}
+                  {...others} />
             </Cell>
           </Grid>
           <Grid style={{paddingTop: 0}}>
@@ -181,35 +190,45 @@ export class SourceFullSummary extends React.Component<SourceFullSummaryProps, S
                   <Grid>
                       <Cell className="line-chart" col={12} tablet={8} phone={6} style={{height: 300}}>
                           <SourceTimeSummary
-                            {...others}
-                            lines={lines}/>
+                              {...others}
+                              lines={lines}/>
                       </Cell>
                       <Cell className="line-chart" col={12} tablet={8} phone={6} style={{height: 300}}>
                         <SourceResponseTimeAverage
-                          {...others}
-                          interval={5}/>
+                            {...others}
+                            interval={5}/>
                       </Cell>
                   </Grid>
               </Cell>
               <Cell style={{marginTop: 0}} col={6} tablet={8} phone={6}>
                   <Grid>
                       {this.props.source && this.props.source.monitoring_enabled &&
-                      <Cell className="line-chart thin-stroke" col={12} tablet={8} phone={6} style={{height: showUpTime ? 300 : 0}}>
-                          <SourceUpTimeSummary handleShowUpTime={handleShowUpTime}
-                            {...others}/>
-                      </Cell>}
+                      (
+                          <Cell className="line-chart thin-stroke" col={12} tablet={8} phone={6}
+                                style={{height: showUpTime ? 300 : 0}}>
+                              {showEmptyGraph ?
+                                  <div className="empty-graph">
+                                      <div className="empty-graph-content">Monitoring data coming soon</div>
+                                  </div> : "" }
+                              (
+                              <SourceUpTimeSummary handleShowEmptyGraph={handleShowEmptyGraph}
+                                                   handleShowUpTime={handleShowUpTime}
+                                                   {...others}/>
+                              )
+                          </Cell>
+                      )}
                       <Cell className="bar-chart" col={12}>
                         <SourceIntentSummary
-                          {...others}
-                          bars={bars}/>
+                            {...others}
+                            bars={bars}/>
                       </Cell>
                   </Grid>
               </Cell>
           </Grid>
         </span>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
 
 export default SourceFullSummary;
--- src/pages/sourcepage/SourceUptime.tsx
+++ src/pages/sourcepage/SourceUptime.tsx
@@ -1,9 +1,9 @@
 import * as moment from "moment";
 import * as React from "react";
 
-import UpTimeChart, { UpTimeData } from "../../components/Graphs/Line/UpTimeChart";
+import UpTimeChart, {UpTimeData} from "../../components/Graphs/Line/UpTimeChart";
 import * as LoadingComponent from "../../components/LoadingComponent";
-import Query, { EndTimeValueParameter, StartTimeValueParameter } from "../../models/query";
+import Query, {EndTimeValueParameter, StartTimeValueParameter} from "../../models/query";
 import Source from "../../models/source";
 import MonitoringService from "../../services/monitoring";
 import SourceUtils from "../../utils/Source";
@@ -13,6 +13,7 @@ interface SourceUpTimeProps extends LoadingComponent.LoadingComponentProps {
     startDate: moment.Moment;
     endDate: moment.Moment;
     handleShowUpTime?: any;
+    handleShowEmptyGraph?: any;
 }
 
 interface SourceUpTimeState extends LoadingComponent.LoadingComponentState<{ summary: UpTimeData[], status: number }> {
@@ -27,7 +28,7 @@ export class SourceUpTime extends LoadingComponent.Component<{ summary: UpTimeDa
     };
 
     constructor(props: SourceUpTimeProps) {
-      super(props, {data: {summary: [], status: 1}} as SourceUpTimeState);
+        super(props, {data: {summary: [], status: 1}} as SourceUpTimeState);
     }
 
     shouldUpdate(oldProps: SourceUpTimeProps, newProps: SourceUpTimeProps) {
@@ -41,11 +42,11 @@ export class SourceUpTime extends LoadingComponent.Component<{ summary: UpTimeDa
     }
 
     preLoad(props: SourceUpTimeProps) {
-        return this.mapState({ data: [], sourceState: 1});
+        return this.mapState({data: [], sourceState: 1});
     }
 
     async startLoading(props: SourceUpTimeProps): Promise<any> {
-        const { source, startDate, endDate } = props;
+        const {source, startDate, endDate} = props;
 
         if (!source) {
             return [];
@@ -55,17 +56,23 @@ export class SourceUpTime extends LoadingComponent.Component<{ summary: UpTimeDa
         query.add(new StartTimeValueParameter(startDate.valueOf()));
         query.add(new EndTimeValueParameter(endDate.valueOf()));
         try {
-          const [sourceStatus, sourcePings] = await Promise.all([MonitoringService.getSourceStatus(source.id), MonitoringService.getUpTimeSummary(query, source.id)]);
-          this.props.handleShowUpTime && this.props.handleShowUpTime(sourceStatus && sourcePings.length > 0);
-          return sortUpTimeSummary(formatUpTimeSummary({ summary: sourcePings, status: sourceStatus.status === "up" ? 1 : 0}));
+            const [sourceStatus, sourcePings] = await Promise.all([MonitoringService.getSourceStatus(source.id), MonitoringService.getUpTimeSummary(query, source.id)]);
+            this.props.handleShowUpTime && this.props.handleShowUpTime(sourceStatus && sourcePings.length > 0);
+            console.log(sourceStatus && sourcePings.length > 0);
+            this.props.handleShowEmptyGraph && this.props.handleShowEmptyGraph(!(sourceStatus && sourcePings.length > 0));
+            return sortUpTimeSummary(formatUpTimeSummary({
+                summary: sourcePings,
+                status: sourceStatus.status === "up" ? 1 : 0
+            }));
         } catch (err) {
-          this.props.handleShowUpTime && this.props.handleShowUpTime(false);
-          return sortUpTimeSummary(formatUpTimeSummary({ summary: [], status: 0}));
+            this.props.handleShowUpTime && this.props.handleShowUpTime(false);
+            this.props.handleShowEmptyGraph && this.props.handleShowEmptyGraph(true);
+            return sortUpTimeSummary(formatUpTimeSummary({summary: [], status: 0}));
         }
     }
 
     map(data: MonitoringService.UpTimeSummary[]): MonitoringService.UpTimeSummary[] {
-      return data;
+        return data;
     }
 
     onLoadError(err: Error) {
@@ -73,27 +80,34 @@ export class SourceUpTime extends LoadingComponent.Component<{ summary: UpTimeDa
     }
 
     render() {
-        const { data } = this.state;
+        const {data} = this.state;
         return (
             <UpTimeChart
                 startDate={this.props.startDate}
                 endDate={this.props.endDate}
                 data={data.summary}
-                status={data.status} />
+                status={data.status}/>
         );
     }
 }
 
 export default SourceUpTime;
 
-function formatUpTimeSummary(result: { summary: MonitoringService.UpTimeSummary[], status: number}): { summary: any, status: number} {
-  const data = result.summary.map((item, i) => {
-    return { source: item.source, status: item.status, timestamp: item.timestamp, statusValue: item.status === "up" ? 1 : 0 };
-  });
-  return { summary: data, status: result.status };
+function formatUpTimeSummary(result: { summary: MonitoringService.UpTimeSummary[], status: number }): { summary: any, status: number } {
+    const data = result.summary.map((item, i) => {
+        return {
+            source: item.source,
+            status: item.status,
+            timestamp: item.timestamp,
+            statusValue: item.status === "up" ? 1 : 0
+        };
+    });
+    return {summary: data, status: result.status};
 }
 
-function sortUpTimeSummary(result: { summary: MonitoringService.UpTimeSummary[], status: number}): { summary: any, status: number}  {
-  const data = result.summary.sort((a, b) => {return a.timestamp - b.timestamp; });
-  return { summary: data, status: result.status };
+function sortUpTimeSummary(result: { summary: MonitoringService.UpTimeSummary[], status: number }): { summary: any, status: number } {
+    const data = result.summary.sort((a, b) => {
+        return a.timestamp - b.timestamp;
+    });
+    return {summary: data, status: result.status};
 }
