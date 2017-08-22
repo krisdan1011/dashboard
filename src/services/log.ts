3b689358da642c82fa4d2a78bffe0c667b0871dc
krisdan1011 <krisdan001011@gmail.com>
2017-08-22 18:52:22 -0500

    Release 1.0.6 (#422)
    * dist-builds
    
    * 1.0.5
    
    * update bower

--- src/services/log.ts
+++ src/services/log.ts
@@ -54,7 +54,7 @@ namespace LogService {
     // let BASE_URL = LOGLESS_BASE; // TODO: Get this to work with Mocha
     export const DEFAULT_TRANSACTIONS_BEFORE = 25;
     export const DEFAULT_TRANSACTIONS_AFTER = 25;
-    const BASE_URL = "https://logless-dev.bespoken.tools/v1";
+    const BASE_URL = "https://logless.bespoken.tools/v1";
     // const BASE_URL = "https://logless-dev.bespoken.tools/v1";
 
     export function getTransactionUrl(convoId: string, transactionsBefore: number, transactionsAfter: number): string {
