81d45062e3399d15c340e3c3edb95d4306fcdba8
krisdan1011 <krisdan001011@gmail.com>
2017-10-09 17:24:14 -0500

    WelcomePage: fix links to match new bespoken domain (#462)

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,4 +1,4 @@
-﻿import * as React from "react";
+import * as React from "react";
 
 import { Cell, Grid } from "../components/Grid";
 
@@ -12,16 +12,16 @@ export default class WelcomePage extends React.Component<any, any> {
                 <Cell col={12}>
                     <h5>News</h5>
                     <ul>
-                        <li><a target="_blank" href="https://bespoken.tools/blog/2016/09/28/developing-locally-with-bst">Developing Locally with BST</a></li>
-                        <li><a target="_blank" href="https://bespoken.tools/blog/2016/11/02/lambda-deploy-and-more">One Step Lambda Deploy</a></li>
-                        <li><a target="_blank" href="https://bespoken.tools/blog/2016/10/10/unit-testing-alexa-skills">Unit Testing Alexa Skills</a></li>
+                        <li><a target="_blank" href="https://bespoken.io/blog/developing-locally-bespoken-tools/">Developing Locally with BST</a></li>
+                        <li><a target="_blank" href="https://bespoken.io/blog/one-step-lambda-deploy/">One Step Lambda Deploy</a></li>
+                        <li><a target="_blank" href="https://bespoken.io/blog/2016/10/10/unit-testing-alexa-skills">Unit Testing Alexa Skills</a></li>
                     </ul>
                 </Cell>
                 <Cell col={12}>
                     <h5>Docs</h5>
                     <ul>
-                        <li><a target="_blank" href="http://docs.bespoken.tools/en/latest/api/classes/logless.html">Setup your Lambda or Cloud Function for Logless</a></li>
-                        <li><a target="_blank" href="http://docs.bespoken.tools/en/latest/tutorials/tutorial_lambda_local/">[Tutorial] Running Lambdas Locally</a></li>
+                        <li><a target="_blank" href="http://docs.bespoken.io/en/latest/api/classes/logless.html">Setup your Lambda or Cloud Function for Logless</a></li>
+                        <li><a target="_blank" href="http://docs.bespoken.io/en/latest/tutorials/tutorial_lambda_local/">[Tutorial] Running Lambdas Locally</a></li>
                     </ul>
                 </Cell>
                 <Cell col={12}>
