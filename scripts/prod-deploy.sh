77b566bdc48c90130a94060e38ecc58ac89426cc
krisdan1011 <krisdan001011@gmail.com>
2017-11-28 15:44:35 -0500

    test

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -6,5 +6,6 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 npm run build
 ls
-git checkout master
-git add . && git commit -m "$0" && git push origin master
+git checkout release
+git add . && git commit -m "$0" && git push origin release
+curl -X POST -d '{"title":"test","head":"release","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
