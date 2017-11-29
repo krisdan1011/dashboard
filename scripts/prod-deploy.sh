2c83c09329eab06e1b3b9955125193dbc003d8c6
krisdan1011 <krisdan001011@gmail.com>
2017-11-29 12:44:02 -0500

    test

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -1,4 +1,4 @@
 echo $TRAVIS_TAG
 git checkout -b release
 git add . && git commit -m "$TRAVIS_TAG" && git push origin release
-curl -X POST -d '{"title":"test","head":"release","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
+curl -X POST -d '{"title":"$TRAVIS_TAG","head":"release","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
