4db9da9411c551e57e79b90b5d4e1d486f7e6586
krisdan1011 <krisdan001011@gmail.com>
2017-11-29 09:22:29 -0500

    test

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -1,12 +1,1 @@
-echo "tag: $0"
-echo $1
-git config --global user.email "rchristian.ramon@gmail.com"
-git config --global user.name "Travis-CI"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-npm run build
-ls
-git checkout -b release
-git add . && git commit -m "$0" && git push origin release
-curl -X POST -d '{"title":"test","head":"release","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
-echo "ok"
+echo $TRAVIS_TAG
