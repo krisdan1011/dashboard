3476ebf4abd30bf154bafd1c15748dc071445220
krisdan1011 <krisdan001011@gmail.com>
2017-11-30 17:28:00 -0500

    test

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -1,11 +1,12 @@
 version=$(echo $TRAVIS_TAG | cut -d'-' -f 2)
+branch=$(echo "release-$version")
 echo "TRAVIS_TAG: $TRAVIS_TAG"
-echo "GH_TOKEN: $GH_TOKEN"
 echo "version: $version"
+echo "branch: $branch"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-git checkout -b release
-git add . && git commit -m "${version}" && git push origin release
+git checkout -b $branch
+git add . && git commit -m "${version}" && git push origin $branch
 git tag $version
 git push origin $version
-curl -X POST -d '{"title":"'"$version"'","head":"release","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
+curl -X POST -d '{"title":"'"$version"'","head":"'"$branch"'","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
