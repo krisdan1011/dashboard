e3092cfa83c0c96b011d065a060efe475ee21409
krisdan1011 <krisdan001011@gmail.com>
2017-11-30 10:46:27 -0500

    test

--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -4,7 +4,8 @@ git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git checkout -b release
 git add . && git commit -m "${TRAVIS_TAG}" && git push origin release
+git tag -d $TRAVIS_TAG
 git push origin :refs/tags/$TRAVIS_TAG
-git tag -fa $TRAVIS_TAG
-git push origin master --tags
+git tag $TRAVIS_TAG
+git push origin $TRAVIS_TAG
 curl -X POST -d '{"title":"'"$TRAVIS_TAG"'","head":"release","base":"master"}' -H "Content-Type: application/json" -H "Authorization: token ${GH_TOKEN}" https://api.github.com/repos/chris-ramon/dashboard/pulls
